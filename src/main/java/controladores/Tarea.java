56aa8769c640efa05d4708c410f4fb45fd990dc7
Personal <Personal@SOGAPRCMRFSD408.FormacionSena.RED>
2025-04-24 11:47:03 -0500

    se agrego codigo en tarea.java

--- src/main/java/controladores/Tarea.java
+++ src/main/java/controladores/Tarea.java
@@ -9,5 +9,37 @@ package controladores;
  * @author Personal
  */
 public class Tarea {
+    private int id;
+    private String  descripcion;
+    private boolean completada;
     
+    public Tarea(int id, String descripcion) {
+        this.id=id;
+        this.descripcion=descripcion;
+        this.completada=false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
 }
